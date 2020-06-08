3898386f4b89b46a55b316a1a00bce73bab93261
j-farkas <jaredmfarkas@gmail.com>
2020-06-07 19:58:36 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,3 @@
-function parse(num){
-  var arr = [];
-  for(var i = 0;i<num;i++)
-  {
-    var j = i.toString().split('');
-    if(j.includes("3"))
-    {
-      arr.push("I'm sorry, Dave. I'm Afraid I can't do that");
-    }else if(j.includes("2")){
-      arr.push("Boop");
-    }else if(j.includes("1")){
-      arr.push("Beep!");
-    }else{
-      arr.push(j.join(''));
-    }
-  }
-  return arr;
-}
-
 var game = new Board();
 function Board(){
   this.gameBoard = fillBoard();
